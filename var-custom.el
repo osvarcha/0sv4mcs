9158bbf4c26f04b930e7eb2ef21c8ff654aaa1c1
OSCAR JEAN FRANCO VARGAS-CHAVEZ <osvarcha@hotmail.com>
2023-02-24 10:39:32 -0500

    fix guix and org

--- var-custom.el
+++ var-custom.el
@@ -1,13 +1,14 @@
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("9111c69f71ecc7140144699c560d09655f3578a08e4ee9723ea095506aa82c63" default)))
+   '("f5b3fff2d5b55d8b69ef38f722fdeef0b9001d74453f24b7bb99da6101a84521" default)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(org-block ((t (:inherit fixed-pitch :background "black" :foreground "white")))))
